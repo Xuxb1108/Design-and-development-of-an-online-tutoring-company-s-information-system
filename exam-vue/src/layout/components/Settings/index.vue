96e7ec99e3e93346052c3fe84990bc4595567f03
等待夏天 <xiaoquan328@163.com>
2023-10-02 01:35:16 +0000

    update exam-vue/src/layout/components/Settings/index.vue. 更新index.vue文件
    Signed-off-by: 等待夏天 <xiaoquan328@163.com>

--- exam-vue/src/layout/components/Settings/index.vue
+++ exam-vue/src/layout/components/Settings/index.vue
@@ -7,7 +7,6 @@
         <span>Theme Color</span>
         <theme-picker style="float: right;height: 26px;margin: -3px 8px 0 0;" @change="themeChange" />
       </div>
-
       <div class="drawer-item">
         <span>Open Tags-View</span>
         <el-switch v-model="tagsView" class="drawer-switch" />
